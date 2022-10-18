688f5a7035f4401e38e62a9e983d968a3b05d6bb
Juan Pablo Ortiz <ortizjup@outlook.com>
2022-10-18 00:41:22 -0300

    more changes

--- BlackJack.NetCore.Web.Api/WeatherForecast.cs
+++ BlackJack.NetCore.Web.Api/WeatherForecast.cs
@@ -1,4 +1,6 @@
-namespace BlackJack.Api
+using System;
+
+namespace BlackJack.NetCore.Web.Api
 {
     public class WeatherForecast
     {
@@ -8,6 +10,6 @@ namespace BlackJack.Api
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
